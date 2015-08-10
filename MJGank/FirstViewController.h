834794935bd43f3f24e3a0f12d915dd9a056e76c
WangMinjun <15895899337@163.com>
2015-08-10 15:24:58 +0800

    Initial Commit

---
+++ MJGank/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MJGank
+//
+//  Created by WangMinjun on 15/8/10.
+//  Copyright (c) 2015年 WangMinjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ MJGank/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MJGank
+//
+//  Created by WangMinjun on 15/8/10.
+//  Copyright (c) 2015年 WangMinjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
